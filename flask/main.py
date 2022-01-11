a40a752b38d694f54d38afcf6759f03e560e10e6
Harshal Gajjar <harshalg98@gmail.com>
2022-01-11 16:43:15 -0500

    flask updated

--- flask/main.py
+++ flask/main.py
@@ -47,22 +47,74 @@ def process():
     inputQuery1=inputQuery1.split("\n")
 
     #Standarization
-    lines=[line.replace("\n","").replace("E-mail","") for line in inputQuery1] #.replace("\t","")
+    raw_lines=[line.replace("\n","").replace("\r","").replace("E-mail","") for line in inputQuery1] #.replace("\t","")
+
+    lines=[]
+
+    while(raw_lines[0]==""):
+        raw_lines.pop(0)
+
+    while(raw_lines[-1]==""):
+        raw_lines.pop(0)
+
+    for line in raw_lines:
+        if(("Waitlist Position" not in line) and ("Notification Expires" not in line)):
+            lines.append(line)
+
     # print(lines)
 
-# %%
-    courses=lines[0::13]
-    terms=[line.replace("\t"," ") for line in lines[1::13]]
-    CRNs=[line.replace("\t"," ") for line in lines[2::13]]
-    status = [line.replace("\t"," ") for line in lines[3::13]]
-    profs = [line.replace("\t"," ") for line in lines[4::13]]
-    gradeModes = [line.replace("\t"," ") for line in lines[5::13]]
-    credits = [line.replace("\t"," ") for line in lines[6::13]]
-    levels = [line.replace("\t"," ") for line in lines[7::13]]
-    campus = [line.replace("\t"," ") for line in lines[8::13]]
+    coursesInfo={}
+    currentCourse=""
+    scheduledMeetingTimes=0
 
-    keys=[line.split("\t") for line in lines[10::13]]
-    values=[line.split("\t") for line in lines[11::13]]
+    for i in range(len(lines)):
+        if(lines[i].strip()==""):
+            continue
+
+        if(i==0 or lines[i-1].strip()==""):
+            currentCourse=lines[i]
+            coursesInfo[currentCourse]={}
+            scheduledMeetingTimes=0
+            coursesInfo[currentCourse]["Meetings"]=[]
+
+        if(scheduledMeetingTimes==0):
+            if("Associated Term:" in lines[i]):
+                coursesInfo[currentCourse]["Associated Term"]=lines[i]
+            elif("CRN:" in lines[i]):
+                coursesInfo[currentCourse]["CRN"]=lines[i]
+            elif("Status:" in lines[i]):
+                coursesInfo[currentCourse]["Status"]=lines[i]
+            elif("Assigned Instructor:" in lines[i]):
+                coursesInfo[currentCourse]["Assigned Instructor"]=lines[i]
+            elif("Grade Mode:" in lines[i]):
+                coursesInfo[currentCourse]["Grade Mode"]=lines[i]
+            elif("Credits:" in lines[i]):
+                coursesInfo[currentCourse]["Credits"]=lines[i]
+            elif("Level:" in lines[i]):
+                coursesInfo[currentCourse]["Level"]=lines[i]
+            elif("Campus:" in lines[i]):
+                coursesInfo[currentCourse]["Campus"]=lines[i]
+            elif("Scheduled Meeting Times" in lines[i]):
+                scheduledMeetingTimes+=1
+                # print("scheduledMeetingTimes++")
+        elif(scheduledMeetingTimes==1):
+            scheduledMeetingTimes+=1
+        else:
+            coursesInfo[currentCourse]["Meetings"].append(lines[i])
+
+# %%
+    # courses=lines[0::13]
+    # terms=[line.replace("\t"," ") for line in lines[1::13]]
+    # CRNs=[line.replace("\t"," ") for line in lines[2::13]]
+    # status = [line.replace("\t"," ") for line in lines[3::13]]
+    # profs = [line.replace("\t"," ") for line in lines[4::13]]
+    # gradeModes = [line.replace("\t"," ") for line in lines[5::13]]
+    # credits = [line.replace("\t"," ") for line in lines[6::13]]
+    # levels = [line.replace("\t"," ") for line in lines[7::13]]
+    # campus = [line.replace("\t"," ") for line in lines[8::13]]
+
+    # keys=[line.split("\t") for line in lines[10::13]]
+    # values=[line.split("\t") for line in lines[11::13]]
     # values
 
     # %%
@@ -81,33 +133,38 @@ def process():
     # %%
     eventInfos=[]
 
-    for i in range(len(courses)):
+    for i in range(len(coursesInfo.keys())):
+        courseName = list(coursesInfo.keys())[i]
 
-        eventInfo={}
+        for j in range(len(coursesInfo[courseName]["Meetings"])):
+            eventInfo={}
 
-        firstStart=values[i][1].split("-")[0].strip()+" "+values[i][4].split("-")[0].strip()
-        firstEnd=values[i][1].split("-")[1].strip()+" "+values[i][4].split("-")[0].strip()
-        # print(firstStart, firstEnd)
-        firstStart=parser.parse(firstStart)
-        firstEnd=parser.parse(firstEnd)
-        # print(firstStart.strftime("%w"))
+            values = coursesInfo[courseName]["Meetings"][j].split("\t")
 
-        while(firstStart.strftime("%w") not in getDays(values[i][2])):
-            # print(firstStart.strftime("%w"))
-            firstStart+=datetime.timedelta(days=1)
-            firstEnd+=datetime.timedelta(days=1)
+            firstStart=values[1].split("-")[0].strip()+" "+values[4].split("-")[0].strip()
+            firstEnd=values[1].split("-")[1].strip()+" "+values[4].split("-")[0].strip()
 
-        courseTill=parser.parse(values[i][4].split("-")[1].strip())
+            firstStart=parser.parse(firstStart)
+            firstEnd=parser.parse(firstEnd)
 
-        eventInfo["SUMMARY"] = courses[i]
-        eventInfo["UID"] = uuid.uuid1().__str__()
-        eventInfo["LOCATION"]=values[i][3]
-        eventInfo["DTSTART;TZID=America/New_York"]=formattedDate(firstStart)
-        eventInfo["DTEND;TZID=America/New_York"]=formattedDate(firstEnd)
-        eventInfo["RRULE"]="FREQ=WEEKLY;UNTIL="+formattedDate(courseTill)+";BYDAY="+getDaysICS(values[i][2])
-        eventInfo["DESCRIPTION"]=(", ".join([re.sub('[^A-Za-z0-9 -]+', '', a[i].replace(":"," -")) for a in [terms, CRNs, status, profs, gradeModes, credits, levels, campus]]))
+            while(firstStart.strftime("%w") not in getDays(values[2])):
+                # print(firstStart.strftime("%w"))
+                firstStart+=datetime.timedelta(days=1)
+                firstEnd+=datetime.timedelta(days=1)
 
-        eventInfos.append(eventInfo)
+            courseTill=parser.parse(values[4].split("-")[1].strip())
+            
+            eventInfo["SUMMARY"] = courseName
+            eventInfo["UID"] = uuid.uuid1().__str__()
+            eventInfo["LOCATION"]=values[3]
+            eventInfo["DTSTART;TZID=America/New_York"]=formattedDate(firstStart)
+            eventInfo["DTEND;TZID=America/New_York"]=formattedDate(firstEnd)
+            eventInfo["RRULE"]="FREQ=WEEKLY;UNTIL="+formattedDate(courseTill)+";BYDAY="+getDaysICS(values[2])
+
+            descKeys = list(filter(lambda x:type(coursesInfo[courseName][x])==str, list(coursesInfo[courseName].keys())))
+            eventInfo["DESCRIPTION"]=(", ".join([re.sub('[^A-Za-z0-9 -]+', '', coursesInfo[courseName][a].replace(":"," - ")) for a in descKeys])).strip()
+
+            eventInfos.append(eventInfo)
 
     # %%
     icsLines=[]
